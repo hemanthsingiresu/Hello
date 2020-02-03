29e6167b5e3965d4619869ce6bbddf988109cd44
hemanthsingiresu <33141377+hemanthsingiresu@users.noreply.github.com>
2020-02-02 21:03:39 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello 
 {
    public static void main(String[] args){
+      //added project
 
          for (int i =1; i <=10; i++){
            System.out.println("Hello world!..."+i);
